fa4d433b2d18446ea89c1d30ff5ef72aedf0fe5d
Khilan Maradiya <maradiyakhilan@gmail.com>
2022-11-05 14:16:33 +0530

    Add loading error component

--- Ecommerse_app/frontend/src/screens/HomeScreen.js
+++ Ecommerse_app/frontend/src/screens/HomeScreen.js
@@ -5,6 +5,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,7 +49,7 @@ function HomeScreen() {
     <h1>Featured Products</h1>
     <div className="products">
       {
-        loading ? <div>Loading...</div> : error ? <div>{error}</div> :
+        loading ? <LoadingBox/> : error ? <MessageBox variant="danger">{error}</MessageBox> :
           (<Row> {products.map((product) => (
             <Col  key={product.slug} sm={6} md={4} lg={3} className="mb-3">
               <Product product={product}></Product>
