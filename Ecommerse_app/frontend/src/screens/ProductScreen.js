f522d63550a2f11a97abab1477d7ff75a4e6f9ef
Khilan Maradiya <maradiyakhilan@gmail.com>
2022-11-05 22:40:34 +0530

    Add cartscreen

--- Ecommerse_app/frontend/src/screens/ProductScreen.js
+++ Ecommerse_app/frontend/src/screens/ProductScreen.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useContext, useEffect, useReducer } from "react";
-import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
@@ -13,6 +12,7 @@ import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
 import { Store } from "../Store";
+import { useNavigate, useParams } from "react-router-dom";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -28,6 +28,7 @@ const reducer = (state, action) => {
 };
 function ProductScreen() {
   const params = useParams();
+  const navigate = useNavigate();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(reducer, {
     loading: true,
@@ -64,6 +65,7 @@ function ProductScreen() {
       type: "CART_ADD_ITEM",
       payload: { ...product, quantity },
     });
+    navigate("/cart");
   };
   return loading ? (
     <LoadingBox />
